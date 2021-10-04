22845af52801f668f1de0152216a9379b50dac98
mcgear <michael.gearhardt@gmail.com>
2021-10-04 08:00:18 -0600

    npm start and html updates

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const apiUrl = `/api/iot-ensemble/WarmQuery?includeEmulated=false`
+const apiUrl = `https://fathym-prd.azure-api.net/iot-ensemble/WarmQuery?includeEmulated=false&lcu-subscription-key=973e1d72ad5d4dd3bc82e0e41f38fbef`
+// const apiUrl = `/api/iot-ensemble/WarmQuery?includeEmulated=false`
 
 const fetchData = () => {
     fetch(apiUrl)
@@ -9,24 +10,26 @@ const fetchData = () => {
             throw new Error('Response not ok.'); 
         })
         .then( dataPack => generateHtml(dataPack))
-        .catch( error => console.error('Error:', error))
+        .catch( error => console.error('Error:', error));
 };
 
 
 const generateHtml = (data) => {
     //console.log(data)
     //console.log(data.Payloads)
-    const myArr = data.Payloads
-    //console.log(myArr[0].SensorReadings.humidity)
-    //console.log(myArr[0].SensorReadings.tempf)
-    const html = `
-        <div class="details">
-            <span>Humidity: ${myArr[0].SensorReadings.humidity}%</span><br />
-            <span>Temp &#176;F: ${myArr[0].SensorReadings.tempf}</span>
-        </div>
-    `
-    const dataDiv = document.querySelector('.dataReadings')
-    dataDiv.innerHTML = html
+    const myArr = data.Payloads;
+    
+    const html = [`<div class="details">`];
+
+    var sensorReadingKeys = Object.keys(myArr[0].SensorReadings || {});
+
+    sensorReadingKeys.forEach(srKey => html.push(`<div><strong>${srKey}</strong>: ${myArr[0].SensorReadings[srKey]}</div>`));
+
+    html.push('</div>');
+
+    const dataDiv = document.querySelector('.dataReadings');
+
+    dataDiv.innerHTML = html.join('');
 }
 
 setInterval(() => {
